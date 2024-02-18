a25fdc0d57cc33016ea321dc60fee391f8f8ebb5
elza <elzaesimoes@gmail.com>
2024-02-18 17:54:49 -0300

    update CAT-TAT.spec.js and src/index.html Exercicios

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -1,5 +1,7 @@
 // <reference types="Cypress" />
 
+const meuTexto = 'O Cypress é uma ferramenta versátil e eficaz para automação de testes front-end. Sua abordagem centrada no desenvolvedor, juntamente com características poderosas, contribui para a criação de testes confiáveis e de alta qualidade. Ao adotar o Cypress, as equipes de desenvolvimento podem melhorar significativamente a eficiência dos testes e garantir a entrega de software robusto e confiável.'
+
 describe("Central de Atendimento ao Cliente TAT", function () {
   beforeEach(() => {
     // Código a ser executado antes de cada teste
@@ -12,29 +14,121 @@ describe("Central de Atendimento ao Cliente TAT", function () {
 
   //utilizar o only para executar apenas o teste que está sendo escrito
 
-  it.only("Envio do formulário", function () {
-    cy.get('input[id="firstName"]')
+  it("Envio do formulário", function () {
+    cy.get('#firstName')
       .should("be.visible")
-      .type("Elza")
+      .type("Elza", { delay: 0})
       .should("have.value", "Elza");
 
-    cy.get('input[id="lastName"]')
+    cy.get('#lastName')
       .should("be.visible")
-      .type("Bisneta")
+      .type("Bisneta", { delay: 0})
       .should("have.value", "Bisneta");
 
-    cy.get('input[id="email"]')
+    cy.get('#email')
       .should("be.visible")
-      .type("elzaesimoes@gmail.com")
+      .type("elzaesimoes@gmail.com", { delay: 0})
       .should("have.value", "elzaesimoes@gmail.com");
 
-    cy.get('textarea[id="open-text-area"]')
-      .should("be.visible")
-      .type("Reembolso")
-      .should("have.value", "Reembolso");
+    cy.get('#open-text-area')
+      .type(meuTexto,  { delay: 0})
+      .should("have.value", meuTexto);
 
     cy.get('button[type="submit"').should("be.visible").click();
 
     cy.get('span[class="success"').should("be.visible");
   });
+
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+    cy.get('#firstName')
+    .should("be.visible")
+    .type("Elza", { delay: 0})
+    .should("have.value", "Elza");
+
+  cy.get('#lastName')
+    .should("be.visible")
+    .type("Bisneta", { delay: 0})
+    .should("have.value", "Bisneta");
+
+  cy.get('#email')
+    .should("be.visible")
+    .type("elzaesimoes", { delay: 0})
+    .should("have.value", "elzaesimoes");
+
+  cy.get('#open-text-area')
+    .type(meuTexto,  { delay: 0})
+    .should("have.value", meuTexto);
+
+  cy.get('button[type="submit"').should("be.visible").click();
+
+  cy.get('span[class="error"').should("be.visible");
+  })
+
+  it('Visto que o campo de telefone só aceita números, crie um teste para validar que, se um valor não-numérico for digitado, seu valor continuará vazio.', function() {
+    cy.wait(5000)
+    cy.get('#phone')
+        .type('abcdeysysa')
+        .should('have.value', '')
+})
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+        cy.get('#firstName')
+        .should("be.visible")
+        .type("Elza", { delay: 0})
+        .should("have.value", "Elza");
+  
+      cy.get('#lastName')
+        .should("be.visible")
+        .type("Bisneta", { delay: 0})
+        .should("have.value", "Bisneta");
+  
+      cy.get('#email')
+        .should("be.visible")
+        .type("elzaesimoes@gmail.com", { delay: 0})
+        .should("have.value", "elzaesimoes@gmail.com");
+  
+      cy.get('#open-text-area')
+        .type(meuTexto,  { delay: 0})
+        .should("have.value", meuTexto);
+  
+      cy.get('button[type="submit"').should("be.visible").click();
+  
+      cy.get('span[class="error"').should("be.visible");
+    })
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
+        cy.get('#firstName')
+        .should("be.visible")
+        .type("Elza", { delay: 0})
+        .clear()
+        .should("have.value", "Elza");
+  
+      cy.get('#lastName')
+        .should("be.visible")
+        .type("Bisneta", { delay: 0})
+        .clear()
+        .should("have.value", "Bisneta");
+  
+      cy.get('#email')
+        .should("be.visible")
+        .type("elzaesimoes@gmail.com", { delay: 0})
+        .clear()
+        .should("have.value", "elzaesimoes@gmail.com");
+  
+      cy.get('#open-text-area')
+        .type(meuTexto,  { delay: 0})
+        .clear()
+        .should("have.value", meuTexto);
+  
+      cy.get('button[type="submit"').should("be.visible").click();
+  
+      cy.get('span[class="error"').should("be.visible");
+    })
+
+    it.only('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function() {
+        cy.get('button[type="submit"').should("be.visible").click();
+        cy.get('span[class="error"').should("be.visible");
+    })
+
 });
+
