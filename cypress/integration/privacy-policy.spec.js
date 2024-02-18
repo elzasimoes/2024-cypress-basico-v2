d192e700bd8fbb2f79c212ac0f007077010e23e4
elza <elzaesimoes@gmail.com>
2024-02-18 20:40:15 -0300

    test: update 1 file and create 1 file

---
+++ cypress/integration/privacy-policy.spec.js
@@ -0,0 +1,24 @@
+// <reference types="Cypress" />
+
+const meuTexto =
+  "Não salvamos dados submetidos no formulário da aplicação CAC TAT. Utilzamos as tecnologias HTML, CSS e JavaScript, para simular uma aplicação real. No entanto, a aplicação é um exemplo, sem qualquer persistência de dados, e usada para fins de ensino. Talking About Testing";
+
+describe("Central de Atendimento ao Cliente TAT", function () {
+  beforeEach(() => {
+    // Código a ser executado antes de cada teste
+    cy.visit("http://127.0.0.1:5500/src/privacy.html"); // Exemplo: visitar um site antes de cada teste
+  });
+
+  it("01 - verifica o título da aplicação", function () {
+    cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT - Política de privacidade");
+  }); 
+
+  it("02 - verifica o título dentro da aplicação", function () {
+    cy.contains('#title', 'CAC TAT - Política de privacidade').should('be.visible')
+  }); 
+
+  it("03 - verifica o texto dentro da aplicação", function () {
+    cy.contains('#white-background', meuTexto).should('be.visible')
+  }); 
+  
+});
